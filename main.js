278c9767bf3565f6c3fe7e148f89767d59bafc08
Pablo Guallichico <pablomp250@gmail.com>
2021-11-21 15:06:45 -0500

    complete the home page

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+window.onscroll = function(){efect_function()}
+var efect_professional_card = document.getElementById("professionals-cards")
+var distance
+function efect_function(){
+    console.log("Estoy en la funcion    ")
+    distance= window.innerHeight - efect_professional_card.getBoundingClientRect().top
+    console.log(distance)
+    if(distance >= 100){
+        console.log("Estoy en el if")
+        efect_professional_card.classList.add("professionals-cards-container__efect")
+    }
+}
+
 document.querySelector('.menu-btn').addEventListener('click',()=>{
     document.querySelector('.nav-section').classList.toggle('show');
 })
