ad02d63f73eaab522aa688e8142931ad6d5287e9
Pablo Guallichico <pablomp250@gmail.com>
2021-11-25 14:46:19 -0500

    Styles in hypnosis page and some changes in navigation bar

--- home_script.js
+++ home_script.js
@@ -2,15 +2,12 @@ window.onscroll = function(){efect_function()}
 var efect_professional_card = document.getElementById("professionals-cards")
 var distance
 function efect_function(){
-    console.log("Estoy en la funcion    ")
     distance= window.innerHeight - efect_professional_card.getBoundingClientRect().top
     console.log(distance)
     if(distance >= 100){
-        console.log("Estoy en el if")
         efect_professional_card.classList.add("professionals-cards-container__efect")
     }
+    else{
+        efect_professional_card.classList.remove("professionals-cards-container__efect")
+    }
 }
-
-document.querySelector('.menu-btn').addEventListener('click',()=>{
-    document.querySelector('.nav-section').classList.toggle('show');
-})
